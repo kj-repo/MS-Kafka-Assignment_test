12aea55541496fbcf1a5d585adf4d171a1801251
kj-repo <67404661+kj-repo@users.noreply.github.com>
2020-07-16 13:40:04 +0530

    added publisher and consumer services

---
+++ assignment_consumer/src/test/java/com/pk/assignment_consumer/AssignmentConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.assignment_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
